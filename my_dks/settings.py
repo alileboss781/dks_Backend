c5c5ef28cd538b24f34455b750859fa399fe5556
badsensey <diallokaou709@gmail.com>
2025-02-13 14:35:24 +0100

    corriger

--- my_dks/settings.py
+++ my_dks/settings.py
@@ -125,3 +125,21 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Indiquez le modèle utilisateur personnalisé
+AUTH_USER_MODEL = 'mon_app.User'
+
+# Logs pour le débogage
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
